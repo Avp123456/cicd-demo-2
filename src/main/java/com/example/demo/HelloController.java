884696629ef77fda187951473002602963ed8428
Akanksha Pawar <129040034+Avp123456@users.noreply.github.com>
2026-08-13 20:04:34 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 	
 	@GetMapping("/")
 	public  String Hello() {
-	return "Hello world I am AP from solapur. updated code final";
+	return "Hello world. updated code final";
 }
 }
